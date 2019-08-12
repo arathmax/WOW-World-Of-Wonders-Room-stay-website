00d796c6c4336b3241a78473ad705cd4f08a38b4
Sunnyskp <skp2june@gmail.com>
2019-08-12 06:05:26 -0400

    roomlist views for GUest and user updated

--- routes/GuestHome.js
+++ routes/GuestHome.js
@@ -1,6 +1,9 @@
 const express = require("express");
-
+const roomSchema = require("../models/Room");
+const mongoose = require("mongoose");
 const router = express.Router();
+
+const Rooms = mongoose.model("rooms", roomSchema);
 
 router.get("/", (req, res) => {
   res.render("Home/homeguest");
@@ -23,6 +26,28 @@ router.get("/admin_login", (req, res) => {
 });
 router.get("/admin_page", (req, res) => {
   res.render("Admin/roomlist");
+});
+
+// GUEST ROOM LIST VIEW
+router.post("/guest_room_list", (req, res) => {
+  Rooms.find({ city: req.body.selectedCity }).then(room => {
+    //user!=null, means that an actual user object was returned
+
+    res.render("Room/guestroomlist", {
+      allRooms: room
+    });
+  });
+});
+
+// USER ROOM LIST VIEW
+router.post("/user_room_list", (req, res) => {
+  Rooms.find({ city: req.body.selectedCity }).then(room => {
+    //user!=null, means that an actual user object was returned
+
+    res.render("Room/userroomlist", {
+      allRooms: room
+    });
+  });
 });
 
 module.exports = router;
